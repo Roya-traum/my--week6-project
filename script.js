5b87d9f6a2323497cfe0ac7586375be8a5382b71
Roya-traum <royasahebnasagh@gmail.com>
2023-05-16 18:00:14 +0330

    update index.html

--- script.js
+++ script.js
@@ -8,9 +8,6 @@ let day = [
   "saturday",
 ];
 
-let apiKey = "85a20eeeb5c60d13f917238c796cb680";
-let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
-
 function showToday(e) {
   let date = new Date();
   let safar = date.getDay();
@@ -24,32 +21,68 @@ function showToday(e) {
     minutes = `0${minutes}`;
   }
 
-  let todayElement = document.querySelector(".today");
-  todayElement.innerHTML = `${dayName} ${hours}:${minutes}`;
+  function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector(".icon");
+
+  celsiusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+  }
+  function search(city) {
+  let apiKey ="85a20eeeb5c60d13f917238c796cb680";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
 }
 
-function setCityName(e) {
-  e.preventDefault();
-  let cityElement = document.querySelector(".city");
-  let cityInputElement = document.querySelector("#cityInput");
-  let tempElement = document.querySelector(".cityTempreture");
-  let CityName = cityInputElement.value;
-  cityElement.innerHTML = CityName;
-  tempElement.innerHTML = "Wait for temp";
-  axios
-    .get(`${apiUrl}${CityName}&appid=${apiKey}&units=metric`)
-    .then(setCityTemp)
-    .catch(function (error) {
-      // handle error
-      tempElement.innerHTML = error.response.data.messages;
-    });
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector(".city-form");
+  search(cityInputElement.value);
 }
 
-function setCityTemp(response) {
-  let tempElement = document.querySelector(".cityTempreture");
-  tempElement.innerHTML = response.data.main.temp;
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
 }
 
-let cityForm = document.querySelector(".cityForm");
-cityForm.addEventListener("submit", setCityName);
-showToday();
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("New York");
